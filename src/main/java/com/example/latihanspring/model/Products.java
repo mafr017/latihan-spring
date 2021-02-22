e6536e5c124b2584bee5f88599bafc7279e06482
M Aditya Fathur Rahman <madityafr3@gmail.com>
2021-02-22 07:18:26 +0700

    Adding validation

--- src/main/java/com/example/latihanspring/model/Products.java
+++ src/main/java/com/example/latihanspring/model/Products.java
@@ -1,16 +1,38 @@
 package com.example.latihanspring.model;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class Products {
 
+    @NotNull(message = "Tidak boleh Kosong!")
+    @Min(1)
     private Long productId;
+    @NotEmpty(message = "Tidak boleh Kosong!")
     private String productName;
+    @NotNull(message = "Tidak boleh Kosong!")
+    @Min(1)
     private Integer supplierId;
+    @NotNull(message = "Tidak boleh Kosong!")
+    @Min(1)
     private Integer categoryId;
+    @NotEmpty(message = "Tidak boleh Kosong!")
     private String quantityperUnit;
+    @NotNull(message = "Tidak boleh Kosong!")
+    @Min(1)
     private Integer unitPrice;
+    @NotNull(message = "Tidak boleh Kosong!")
+    @Min(1)
     private Integer unitsInStock;
+    @NotNull(message = "Tidak boleh Kosong!")
+    @Min(1)
     private Integer unitsOnOrder;
+    @NotNull(message = "Tidak boleh Kosong!")
+    @Min(1)
     private Integer reorderLevel;
+    @NotNull(message = "Tidak boleh Kosong!")
+    @Min(1)
     private Integer discontinued;
 
     public Long getProductId() {
--- src/main/java/com/example/latihanspring/ui/HomeAction.java
+++ src/main/java/com/example/latihanspring/ui/HomeAction.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeAction {
@@ -27,14 +30,14 @@ public class HomeAction {
 
     @GetMapping("/addprd")
     public String addProduct(Model model) {
-        model.addAttribute("Product", new Products());
+        model.addAttribute("products", new Products());
         idPrd = 0L;
         return "addEditprd";
     }
 
     @GetMapping("/updateprd/{id}")
     public String updateProduct(@PathVariable("id") int id, Model model) {
-        model.addAttribute("Product", masterData.fetchProductsJdbcByPrdId(id));
+        model.addAttribute("products", masterData.fetchProductsJdbcByPrdId(id));
         idPrd = Long.valueOf(id);
         return "addEditprd";
     }
@@ -47,13 +50,18 @@ public class HomeAction {
     }
 
     @PostMapping("/saveproduct")
-    public String saveProduct(Products products) {
-        if (idPrd > 0L) {
-            masterData.updateProductJdbc(products);
+    public String saveProduct(@Valid Products products, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+//            System.out.println(bindingResult.toString());
+            return "addEditprd";
         } else {
-            masterData.insertProductJdbc(products);
+            if (idPrd > 0L) {
+                masterData.updateProductJdbc(products);
+            } else {
+                masterData.insertProductJdbc(products);
+            }
+            return "redirect:listprd";
         }
-        return "redirect:listprd";
     }
 
 }
